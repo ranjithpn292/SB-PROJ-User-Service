6d1848e6031620332ab5ef0e5c9b7710db617908
ranjithpn292 <ranjithpn292@gmail.com>
2025-02-09 23:44:58 +0530

    added circuit breakers

--- src/main/java/com/ran/user/service/controllers/UserController.java
+++ src/main/java/com/ran/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.ran.user.service.controllers;
 
 import com.ran.user.service.entities.User;
 import com.ran.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
@@ -25,9 +29,20 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
     ResponseEntity<User> getSingleUser(@PathVariable String userId){
      User user = userService.getUserById(userId);
      return ResponseEntity.ok(user);
+    }
+
+    // creatingf fallack methods for ratinngHotelService circuit
+    public ResponseEntity<User> ratingHotelFallback(String UserId, Exception ex){
+        log.info("Fallback is executed because service is down:", ex.getMessage());
+        User user = new User();
+        user.setName("dummy name");
+        user.setEmail("dummy@gmai.com");
+        user.setAbout("dummy about");
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @GetMapping
